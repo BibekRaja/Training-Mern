548e4eb9b563abb2e45b1b2a012360bb748d077c
BibekRaja <47000134+BibekRaja@users.noreply.github.com>
2023-01-02 16:21:50 +0545

    js

---
+++ js/multiplication.js
@@ -0,0 +1,31 @@
+// first method
+
+// function multiplication(number, iteration) {
+//     for (i = 1; i <= iteration; i++) {
+//         product = number * i;
+//         console.log(number + " x " + i + " = " + product);
+//     }
+// }
+
+// multiplication(10, 100);
+
+
+
+
+
+
+// second method
+multipleOf = 20;
+times = 10;
+
+function product(number, iteration) {
+    pro = number * i;
+    console.log(number + " x " + i + " = " + pro);
+}
+
+
+for (i = 1; i <= times; i++) {
+    product(multipleOf, i);
+}
+
+
